f5963bad1f502bfeed9929b43dbe4840ebfbd7d8
chh817 <chh817@gmail.com>
2022-06-16 17:11:34 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,9 +3,12 @@ const express = require("express")
 
 const router = express.Router()
 
-// Route for login page
+// Routes for login page
 router.get('/login', (req, res) => { res.render('login') })
-
 router.post('/login', (req, res) => { })
+
+// Routes for register page
+router.get('/register', (req, res) => { res.render('register') })
+
 
 module.exports = router
