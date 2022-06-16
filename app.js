f385d8429fb7fa153a09e2f877a132d016b9e111
chh817 <chh817@gmail.com>
2022-06-16 20:49:05 +0800

    feat: add user registration feature

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.static("public"))
 // Use body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
 // Use method-override
 app.use(methodOverride("_method"))
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,12 +3,27 @@ const express = require("express")
 
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // Routes for login page
 router.get('/login', (req, res) => { res.render('login') })
 router.post('/login', (req, res) => { })
 
 // Routes for register page
 router.get('/register', (req, res) => { res.render('register') })
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user)
+        return res.render('register', { name, email, password, confirmPassword })
+      User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => (console.log(err)))
+})
 
 
 module.exports = router
